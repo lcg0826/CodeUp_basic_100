7dae174e7a232a3147f9a2b0e8cdf9f2e9988506
lcg0826 <lightchan0826@gmail.com>
2020-12-07 18:25:01 +0900

    코드업 기초 100제 중 1001 ~ 1020번

--- CodeUpBasic/Codeup_1000_1020/1002.py
+++ CodeUpBasic/Codeup_1000_1020/1002.py
@@ -1,5 +1,5 @@
 # 1002 : [기초-출력] 출력하기02(설명)
 # 이번에는 공백()을 포함한 문장을 출력한다.
 # 다음 문장을 출력해보자.
-# Hello World
+# Hello World 
 print('Hello World')
--- CodeUpBasic/Codeup_1000_1020/1005.py
+++ CodeUpBasic/Codeup_1000_1020/1005.py
@@ -1,4 +1,4 @@
 # 1005 : [기초-출력] 출력하기05(설명)
 # 이번에는 큰따옴표(double quotation mark)가 포함된 출력문을 연습해보자.
 # 다음 문장을 출력하시오.
-print('"Hello World"')
+print('"Hello World"') 
